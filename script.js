09cbc32b1ebdc76156966c6302f5a214a179786b
Firas Jemaa <firas.jemaa1990@gmail.com>
2023-09-10 19:44:01 +0200

    Fin partie principal

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ toggler.addEventListener("click", toggleNav);
 
 
 new ResizeObserver(entries => {
-  if (entries[0].contentRect.width <= 900){
+  if (entries[0].contentRect.width <= 900) {
     navLinksContainer.style.transition = "transform 0.4s ease-out";
   } else {
     navLinksContainer.style.transition = "none";
@@ -29,18 +29,64 @@ new ResizeObserver(entries => {
 /* Scroll */
 window.addEventListener('scroll', reveal);
 
-function reveal(){
-    let reveals = document.querySelectorAll('.reveal');
+function reveal() {
+  let reveals = document.querySelectorAll('.reveal');
 
-    for (let i = 0; reveals.length; i++){
-        let windowheight = window.innerHeight;
-        let revealtop = reveals[i].getBoundingClientRect().top;
-        let revealpoint = 150;
+  for (let i = 0; reveals.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    let revealpoint = 150;
 
-        if(revealtop < windowheight - revealpoint){
-            reveals[i].classList.add('active');
-        /*}else{
-            reveals[i].classList.remove('active');*/
-        }
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
+      /*}else{
+          reveals[i].classList.remove('active');*/
     }
+  }
 };
+
+// formation
+const allRonds = document.querySelectorAll('.rond');
+const allBoxes = document.querySelectorAll('.box');
+
+const controller = new ScrollMagic.Controller()
+
+allBoxes.forEach(box => {
+
+  for (i = 0; i < allRonds.length; i++) {
+
+    if (allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')) {
+      let tween = gsap.from(box, { y: -50, opacity: 0, duration: 0.5 })
+
+      let scene = new ScrollMagic.Scene({
+        triggerElement: allRonds[i],
+        reverse: true
+      })
+        .setTween(tween)
+        .addTo(controller)
+    }
+  }
+})
+
+// BTN scroll top
+window.addEventListener('scroll', TopScroll);
+function TopScroll() {
+  let BtnScrollTop = document.getElementById('topButton');
+    
+  if (window.scrollY > 200){
+    console.log("ca marche");
+    BtnScrollTop.style.visibility ="visible";
+  }else{
+    BtnScrollTop.style.visibility ="hidden";
+    console.log(window.scrollY);
+  }
+};
+
+document
+  .getElementById('topButton')
+  .addEventListener("click", function(){
+    window.scrollTo({
+      top:0,
+      behavior:"smooth"
+    });
+  });
