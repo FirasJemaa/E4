af77c8d668645fad753b519a01f13545ae58b5ec
Firas Jemaa <firas.jemaa@yahoo.fr>
2023-11-29 12:13:58 +0100

    MàJ

--- assets/script/veille.js
+++ assets/script/veille.js
@@ -1,5 +1,4 @@
-//import 'script/js';
-
+// Slide
 document.getElementById('next').onclick = function () {
   let lists = document.querySelectorAll('.item');
   document.getElementById('slide').appendChild(lists[0]);
@@ -9,38 +8,61 @@ document.getElementById('prev').onclick = function () {
   let lists = document.querySelectorAll('.item');
   document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
-//https://www.lemondeinformatique.fr/flux-rss/
-//https://www.lemondeinformatique.fr/flux-rss/thematique/le-monde-du-cloud-computing/rss.xml
-//https://www.zdnet.fr/feeds/rss/
-const rssFeedUrl = 'https://www.zdnet.fr/feeds/rss/';
-let feedContent = document.getElementById("slide");
 
-fetch(`https://api.rss2json.com/v1/api.json?rss_url=${rssFeedUrl}`)
-  .then(response => {
-    const contentType = response.headers.get('content-type');
+//RSS
 
-    if (contentType && contentType.includes('application/json')) {
-      return response.json(); // La réponse est au format JSON
-    } else if (contentType && contentType.includes('application/xml')) {
-      return response.text(); // La réponse est au format XML
-    } else {
-      throw new Error('Type de contenu non pris en charge');
-    }
-  })
-  .then(data => {
-    if (data.status === 'ok') {
-      const items = data.items;
-      let Remplissage = "";
+//Ecoute du changement de veille
+ const Titres = document.getElementsByClassName("soulignage");
+ Array.from(Titres).forEach(function(element) {
+  element.addEventListener('click', function() {
+      Array.from(Titres).forEach(function(el) {
+          //el.classList.remove('soulig');
+          el.id = '';
+      });
+      element.id = 'activeTitre';
+      AffichageRSS();
+  });
+});
 
-      // Affichez les titres, les dates et les liens vers les articles du flux RSS
-      for (const item of items) {
-        const title = item.title;
-        const date = item.pubDate;
-        const description = item.description;
-        const link = item.link;
-        const mediaContent = (item.enclosure.link != undefined) ? `style="background-image: url(${item.enclosure.link});"` : `style="background-image: url('./assets/images/VeilleImgDefault.jpeg');"`;
-        
-        Remplissage += (`
+//Affichage du flux RSS
+function AffichageRSS() {
+  const Titre = document.getElementById("activeTitre");
+  let rssFeedUrl = "";
+
+  if (Titre.innerText === "Blockchain"){
+    rssFeedUrl = 'https://www.zdnet.fr/feeds/rss/';
+  }else{
+    rssFeedUrl = 'https://www.lemondeinformatique.fr/flux-rss/thematique/le-monde-du-cloud-computing/rss.xml';
+  }
+
+  let feedContent = document.getElementById("slide");
+
+  fetch(`https://api.rss2json.com/v1/api.json?rss_url=${rssFeedUrl}`)
+    .then(response => {
+      const contentType = response.headers.get('content-type');
+
+      if (contentType && contentType.includes('application/json')) {
+        return response.json(); // La réponse est au format JSON
+      } else if (contentType && contentType.includes('application/xml')) {
+        return response.text(); // La réponse est au format XML
+      } else {
+        throw new Error('Type de contenu non pris en charge');
+      }
+    })
+    .then(data => {
+      if (data.status === 'ok') {
+        const items = data.items;
+        let Remplissage = "";
+
+        // Affichez les titres, les dates et les liens vers les articles du flux RSS
+        for (const item of items) {
+          const title = item.title;
+          const date = item.pubDate;
+          const description = item.description;
+          const link = item.link;
+          const mediaContent = (item.enclosure.link != undefined) ? `style="background-image: url(${item.enclosure.link});"` : `style="background-image: url('./assets/images/VeilleImgDefault.jpeg');"`;
+
+          Remplissage += (`
             <div class="item" ${mediaContent}">
                     <div class="content">
                         <div class="name">${title}</div>
@@ -51,17 +73,19 @@ fetch(`https://api.rss2json.com/v1/api.json?rss_url=${rssFeedUrl}`)
                         <a href="${link}">Voir plus</a>
                     </div>
             </div> `
-        );
+          );
+        }
+        feedContent.innerHTML = Remplissage;
+      } else {
+        console.error('Erreur lors de la récupération du flux RSS.');
       }
-      feedContent.innerHTML = Remplissage;
-    } else {
-      console.error('Erreur lors de la récupération du flux RSS.');
-    }
-  })
-  .catch(error => {
-    console.error('Erreur lors de la requête AJAX.', error);
-  });
+    })
+    .catch(error => {
+      console.error('Erreur lors de la requête AJAX.', error);
+    });
+}
 
+AffichageRSS();
 /*
 function handleScreenSizeChange(event) {
   let Images = document.querySelectorAll(".item");
